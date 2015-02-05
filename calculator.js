af410d318ad038148fbd57ce9f9a63eccc117c47
BV Satyaram <bvsatyaram@gmail.com>
2015-02-05 18:29:22 +0530

    Handling decimals and backspace

--- calculator.js
+++ calculator.js
@@ -20,15 +20,50 @@ var Calculator = {
     Calculator.appendToPreviewInput(digit);
   },
   inputOperation: function(operation) {
-    Calculator.performCalculation();
-    Calculator.appendToPreviewInput(operation);
-    Calculator.previousOperation = operation;
-    $('#main_input').html(Calculator.defaultText);
+    if ($('#main_input').html() == Calculator.defaultText) {
+      Calculator.previousOperation = operation;
+      var previewString = $('#preview_input').html();
+      $('#preview_input').html(previewString.substring(0, previewString.length - 1));
+      Calculator.appendToPreviewInput(operation);
+    } else {
+      Calculator.performCalculation();
+      Calculator.appendToPreviewInput(operation);
+      Calculator.previousOperation = operation;
+      $('#main_input').html(Calculator.defaultText);
+    }
+  },
+  inputDot: function() {
+    var existingVal = $('#main_input').html();
+    if (existingVal == Calculator.defaultText) {
+      Calculator.inputDigit('0');
+      Calculator.inputDigit('.');
+    } else if (existingVal.match(/\./)) {
+      // Do nothing, as there is already one decimal in the input
+    } else {
+      Calculator.inputDigit('.');
+    }
   },
   setInputs: function(val) {
     $('#main_input').html(val);
     $('#preview_input').html(val);
 
+  },
+  deleteInput: function() {
+    var existingVal = $('#main_input').html();
+    var previewString = $('#preview_input').html();
+    if (existingVal == Calculator.defaultText) {
+      // Nothing to delete
+    } else if (existingVal.length == 1) {
+      $('#main_input').html(Calculator.defaultText);
+      if ($('#preview_input').html().length == 1) {
+        $('#preview_input').html(Calculator.defaultText);
+      } else {
+        $('#preview_input').html(previewString.substring(0, previewString.length - 1));
+      }
+    } else {
+      $('#main_input').html(existingVal.substring(0, existingVal.length - 1));
+      $('#preview_input').html(previewString.substring(0, previewString.length - 1));
+    }
   },
   reset: function() {
     Calculator.setInputs(Calculator.defaultText);
@@ -70,6 +105,14 @@ var Calculator = {
     $('#equals').click(function(){
       var result = Calculator.performCalculation();
       Calculator.setInputs(result);
+    });
+
+    $('#dot').click(function(){
+      Calculator.inputDot();
+    });
+
+    $('#delete').click(function(){
+      Calculator.deleteInput();
     });
 
     $('#reset').click(function() {
